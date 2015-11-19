8b48de07d34b44717877f70724b28da6e6d3e886
Georg Dümmler <gd@bergwerk.ag>
2015-11-19 15:33:57 +0100

    add compatibilty for TYPO3 7.6

--- Classes/Domain/Repository/ViewRepository.php
+++ Classes/Domain/Repository/ViewRepository.php
@@ -40,9 +40,8 @@ class ViewRepository extends \TYPO3\CMS\Extbase\Persistence\Repository
     public function getSiteRoot()
     {
         $query = $this->createQuery();
-        $query->getQuerySettings()->setReturnRawQueryResult(TRUE);
         $query->statement('SELECT * FROM pages WHERE hidden=0 AND deleted=0 AND is_siteroot=1');
-        return $query->execute();
+        return $query->execute(true);
     }
 
     /**
@@ -51,9 +50,8 @@ class ViewRepository extends \TYPO3\CMS\Extbase\Persistence\Repository
     public function getAllPages()
     {
         $query = $this->createQuery();
-        $query->getQuerySettings()->setReturnRawQueryResult(TRUE);
         $query->statement('SELECT * FROM pages WHERE hidden=0 AND deleted=0 AND doktype!=199 AND doktype!=254 AND nav_hide=0 AND is_siteroot=0 ORDER BY pid,sorting,uid');
-        return $query->execute();
+        return $query->execute(true);
     }
 
     /**
@@ -63,8 +61,7 @@ class ViewRepository extends \TYPO3\CMS\Extbase\Persistence\Repository
     public function getAllPagesForPid($pid)
     {
         $query = $this->createQuery();
-        $query->getQuerySettings()->setReturnRawQueryResult(TRUE);
         $query->statement('SELECT * FROM pages WHERE hidden=0 AND deleted=0 AND doktype!=199 AND doktype!=254 AND nav_hide=0 AND pid='.$pid);
-        return $query->execute();
+        return $query->execute(true);
     }
 }
