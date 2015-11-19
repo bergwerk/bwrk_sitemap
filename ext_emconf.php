8eef11ebeb15b54ebcd856fb82a33e19a066099b
Georg Dümmler <gd@bergwerk.ag>
2015-11-19 15:34:43 +0100

    Version Update

--- ext_emconf.php
+++ ext_emconf.php
@@ -39,10 +39,10 @@ $EM_CONF[$_EXTKEY] = array(
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '1.3.1',
+    'version' => '1.4',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '6.2.0-6.2.99',
+            'typo3' => '6.2.0-7.6.99',
         ),
         'conflicts' => array(),
         'suggests' => array(),
