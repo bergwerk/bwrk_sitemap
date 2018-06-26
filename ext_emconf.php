1f2d8df8b135d39df7ddf4679aa94f1ebcdb9d82
Daniel Maier <dm@bergwerk.ag>
2018-06-26 13:51:27 +0200

    [v1.5.2] version increase before archiving this extension

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,40 +1,20 @@
 <?php
 
-/***************************************************************
- * Extension Manager/Repository config file for ext "bwrk_sitemap".
- *
- * Auto generated 02-12-2015 16:55
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
- ***************************************************************/
-
 $EM_CONF[$_EXTKEY] = array (
-  'title' => 'Sitemap',
+  'title' => 'BERGWERK Sitemap',
   'description' => 'Simple TYPO3 Sitemap-Extension which allows you to create a xml-sitemap from your page-tree and external plugins via the TYPO3 Signal-Slot-Dispatcher.',
   'category' => 'plugin',
-  'author' => 'Georg Dümmler, Daniel Maier',
-  'author_email' => 'gd@bergwerk.ag, dm@bergwerk.ag',
+  'author' => 'BERGWERK',
+  'author_email' => 'technik@bergwerk.ag',
   'author_company' => 'BERGWERK Werbeagentur GmbH',
   'state' => 'stable',
-  'uploadfolder' => false,
-  'createDirs' => '',
-  'clearCacheOnLoad' => 0,
-  'version' => '1.5.1',
+  'version' => '1.5.2',
   'constraints' => 
   array (
-    'depends' => 
-    array (
-      'typo3' => '6.2.0-7.6.99',
+    'depends' => array (
+      'typo3' => '6.2.0-7.6.99'
     ),
-    'conflicts' => 
-    array (
-    ),
-    'suggests' => 
-    array (
-    ),
-  ),
-  'clearcacheonload' => false,
+    'conflicts' => array (),
+    'suggests' => array ()
+  )
 );
-
